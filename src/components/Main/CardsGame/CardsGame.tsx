6e409aa30ace31bce86028b334a3dde1700a6ceb
Alireza Maleki <mt@gmail.com>
2023-03-13 16:11:36 +0330

    adding responsive design to cards game

--- src/components/Main/CardsGame/CardsGame.tsx
+++ src/components/Main/CardsGame/CardsGame.tsx
@@ -11,7 +11,7 @@ const [ cardList , SetCardList ] = useState<any>([]);
 const [ canPlay , SetCanPlay ] = useState<boolean>(false);
 
 useEffect(() => {
-    let arr = [ 'animate1' ,'animate2' ,'animate3','animate4','animate5' ,'animate6' ,'animate7' ,'animate8' ,'animate9' ,'animate10' ,'animate1' ,'animate2' ,'animate3','animate4','animate5' ,'animate6' ,'animate7' ,'animate8' ,'animate9' ,'animate10' ];
+    let arr = [ 'animate1' ,'animate2' ,'animate3','animate4','animate5' ,'animate6' ,'animate7' ,'animate8' ,'animate9' ,'animate10' , 'animate10' , 'animate9' , 'animate8' , 'animate7' , 'animate6' , 'animate5', 'animate4' , 'animate3' , 'animate2' , 'animate1'];
 
     const list = arr.map(( item : any) => (
       <Card animate={item}/>
@@ -29,14 +29,14 @@ useEffect(() => {
        { !canPlay && 
 <div className="h-screen flex flex-col justify-start overflow-hidden">
 <Flip left cascade>
-   <p className="text-center my-4 text-[#4c4c4c] text-[2rem] font-[600]">Pick Your Three Cards !</p>
+   <p className="text-center my-4 md:my-2 text-[#4c4c4c] text-[2rem] font-[600]">Pick Your Three Cards !</p>
 </Flip>
 
-      <div className="h-[50%] flex flex-wrap justify-around boxshadow4 ">
+      <div className="h-[50%] md:h-[60%] flex flex-wrap md:pb-[1.5rem] justify-around boxshadow4 ">
         {cardList}
       </div>
 
-    <div className="text-center mt-[2rem] text-[2rem] font-[600] flex flex-row justify-around w-5/12 mx-auto">
+    <div className="text-center mt-[2rem] text-[2rem] font-[600] flex flex-row justify-around w-5/12 md:w-8/12 mx-auto">
       <p className="text-[1.5rem] text-[white] hover:scale-105 duration-200 transition-all bg-[#be4d15] hover:bg-[#892602] h-[2.5rem] cursor-pointer mt-4 px-[2rem] rounded-[20px]">Catch</p>
       <p className="text-[#760e2f] duration-200 transition-all border-[3px] border-solid border-[#af143b] h-[4rem] w-[4rem] pt-1 rounded-[50%]">30</p>
     </div>
--- src/components/Main/CardsGame/PlayForm.tsx
+++ src/components/Main/CardsGame/PlayForm.tsx
@@ -3,13 +3,13 @@ import payment from '../../../images/payment.webp'
 const PlayForm = () => {
     
     return (
-        <div className="flex flex-row justify-around text-[#c7c7c7] h-screen">
+        <div className="flex flex-row md:flex-col justify-around md:justify-center text-[#c7c7c7] h-screen">
             
-             <div className='w-[50%] h-screen border-r-[3px] border-r-solid border-r-[#1f1f1f]  boxshadow2 bg-[#171717]'>
+             <div className='w-[50%] md:w-full md:pt-[2rem] h-screen md:h-[50%] border-r-[3px] border-r-solid border-r-[#1f1f1f]  boxshadow2 bg-[#171717]'>
                 <div className='flex flex-col justify-around h-[80%] pl-5'>
-                  <div className='flex flex-row'><p className='text-[2rem] text-center text-[#7c3719da] w-[4rem] h-[4rem] boxshadow2 pt-1 rounded-[50%] border-[3px] border-solid border-[#c35425da]'>1x</p><p className='text-[#595959] pt-1 px-2 text-[1.5rem]'>----- One Pair card multipy your amount to <span className='text-[#a73f12da] text-[2rem] pl-3 mt-1'>1.2 x</span></p></div>
-                  <div className='flex flex-row'><p className='text-[2rem] text-center text-[#106a81da] w-[4rem] h-[4rem] boxshadow2 pt-1 rounded-[50%] border-[3px] border-solid border-[#25a3c3da]'>2x</p><p className='text-[#595959] pt-1 px-2 text-[1.5rem]'>----- Two Pair card multipy your amount to <span className='text-[#0f6593da] text-[2rem] pl-3 mt-1'>1.5 x</span></p></div>
-                  <div className='flex flex-row'><p className='text-[2rem] text-center text-[#20762eda] w-[4rem] h-[4rem] pt-1 rounded-[50%] boxshadow2 border-[3px] border-solid border-[#43c358da]'>3x</p><p className='text-[#595959] pt-1 px-2 text-[1.5rem]'>----- Three Pair card multipy your amount to <span className='text-[#1e9232da] text-[2rem] pl-3 mt-1'>2 x</span></p></div>
+                  <div className='flex flex-row'><p className='text-[2rem] md:text-[1.5rem] text-center text-[#7c3719da] w-[4rem] h-[4rem] md:w-[3rem] md:h-[3rem] boxshadow2 pt-1 md:pt-0 rounded-[50%] border-[3px] border-solid border-[#c35425da]'>1x</p><p className='text-[#595959] pt-1 md:pt-3 px-2 text-[1.5rem] md:text-[0.9rem]'>----- One Pair card multipy your amount to <span className='text-[#a73f12da] text-[2rem] md:text-[1.5rem] pl-3 mt-1 md:hidden'>1.2 x</span></p></div>
+                  <div className='flex flex-row'><p className='text-[2rem] md:text-[1.5rem] text-center text-[#106a81da] w-[4rem] h-[4rem] md:w-[3rem] md:h-[3rem] boxshadow2 pt-1 md:pt-0 rounded-[50%] border-[3px] border-solid border-[#25a3c3da]'>2x</p><p className='text-[#595959] pt-1 md:pt-3 px-2 text-[1.5rem] md:text-[0.9rem]'>----- Two Pair card multipy your amount to <span className='text-[#0f6593da] text-[2rem] md:text-[1.5rem] pl-3 mt-1 md:hidden'>1.5 x</span></p></div>
+                  <div className='flex flex-row'><p className='text-[2rem] md:text-[1.5rem] text-center text-[#20762eda] w-[4rem] h-[4rem] md:w-[3rem] md:h-[3rem] pt-1 md:pt-0 rounded-[50%] boxshadow2 border-[3px] border-solid border-[#43c358da]'>3x</p><p className='text-[#595959] pt-1 md:pt-3 px-2 text-[1.5rem] md:text-[0.9rem]'>----- Three Pair card multipy your amount to <span className='text-[#1e9232da] text-[2rem] md:text-[1.5rem] pl-3 mt-1 md:hidden'>2 x</span></p></div>
                 </div>
             </div>
             
@@ -18,22 +18,22 @@ const PlayForm = () => {
            
           
            
-            <div className='w-[55%] flex flex-col justify-center h-[75%] '>
+            <div className='w-[55%] md:w-full flex flex-col justify-center md:justify-start h-[75%] md:h-[85%]'>
              
-              <p className='text-[#a3a3a3] font-[600]  text-center mb-[5rem] text-[2rem]'>Card Game</p>
+              <p className='text-[#a3a3a3] font-[600]  text-center mb-[5rem] md:mb-[2rem] md:mt-5 text-[2rem] md:text-[1.5rem]'>Card Game</p>
 
               <div className='flex flex-row'>
-                <div className='w-[70%] text-center text-[2rem]'>
-                   <p className='w-[70%] mt-[4rem] py-5 pb-6 mx-auto rounded-[10px] bg-[#0e0e0e] boxshadow2'>Balance : 385 <span className='text-[green]'>$</span></p>
+                <div className='w-[70%] text-center text-[2rem] md:text-[1.5rem]'>
+                   <p className='w-[70%] mt-[4rem] md:mt-2 py-5 pb-6 mx-auto rounded-[10px] bg-[#0e0e0e] boxshadow2'>Balance : 385 <span className='text-[green]'>$</span></p>
                 </div> 
-                <div className='w-[30%] mt-3 mr-5 mx-auto opacity-[0.8]'><img src={payment} /></div> 
+                <div className='w-[30%] md:w-[28%] mt-3 md:mt-0 mr-5 mx-auto opacity-[0.8]'><img src={payment} /></div> 
               </div>
             
               <div className='flex flex-row mt-5'>
-                <div className='w-[70%] text-center text-[2rem]'>
-                   <input className='w-[70%]  py-5 mx-auto pb-6 rounded-[10px] bg-[#0e0e0e] boxshadow2 pl-[2rem] outline-none placeholder:text-[#414141]' placeholder='Amount'></input>
+                <div className='w-[70%] text-center text-[2rem] md:text-[1.5rem]'>
+                   <input className='w-[70%] py-5 mx-auto pb-6 rounded-[10px] bg-[#0e0e0e] boxshadow2 pl-[2rem] md:pl-[1rem] outline-none placeholder:text-[#414141]' placeholder='Amount'></input>
                 </div> 
-                <div className='w-[30%] mx-auto '><p className=' w-[5rem] pt-5 text-[1.5rem] h-[5rem] rounded-[50%] boxshadow2  font-[600] bg-[#a93e01] hover:bg-[#9f2f0f] text-center ml-[3rem] cursor-pointer hover:scale-105 transition-all duration-200 '>Play</p></div> 
+                <div className='w-[30%] mx-auto '><p className=' w-[5rem] pt-5 text-[1.5rem] h-[5rem] rounded-[50%] boxshadow2  font-[600] bg-[#a93e01] hover:bg-[#9f2f0f] text-center ml-[3rem] md:ml-0 cursor-pointer hover:scale-105 transition-all duration-200 '>Play</p></div> 
               </div>
 
             </div>
