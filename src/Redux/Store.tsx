fe18d1701c784f3cc6f200e54250281f5f71bac4
Alireza Maleki <mt@gmail.com>
2023-03-27 15:04:27 +0430

    adding profile image ui

--- src/Redux/Store.tsx
+++ src/Redux/Store.tsx
@@ -1,8 +1,10 @@
-import { configureStore } from '@reduxjs/toolkit'
-import CardsGame from './Reducers/CardsGame/CardsGame'
+import { configureStore } from '@reduxjs/toolkit';
+import CardsGame from './Reducers/CardsGame/CardsGame';
+import ProfileSettings from './Reducers/Settings/Profile/ProfileSettings';
 
 export default configureStore({
   reducer: {
-    CardsGame 
+    CardsGame ,
+    ProfileSettings
   },
 })
