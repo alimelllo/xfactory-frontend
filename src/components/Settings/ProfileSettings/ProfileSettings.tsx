fc936ecdbbf3009f972751ffe3f7d70539c93a5e
alimelllo <ali.melllo@yahoo.com>
2023-04-04 07:36:16 +0330

    refactoring....

---
+++ src/components/Settings/ProfileSettings/ProfileSettings.tsx
@@ -0,0 +1,78 @@
+import { profileImageHandler } from '../../../Redux/Reducers/Settings/Profile/ProfileSettings';
+import { useDispatch, useSelector } from "react-redux";
+import userService from "../../../api/services/user.service";
+import jwt_decode from "jwt-decode";
+import { useEffect, useState } from "react";
+
+
+const ProfileSettings = () => {
+
+    const selectState :any = useSelector(profileImageHandler);
+    const profileImageState = selectState.payload.ProfileSettings.profileImage;
+    
+    const SetProfileImage = useDispatch();
+    
+    const getBase64 = ( file : any ) => {
+        return new Promise(resolve => {
+          let baseURL = "";
+          let reader = new FileReader();
+          reader.readAsDataURL(file);
+    
+          reader.onload = () => {
+            resolve(baseURL);
+            let {result} : any = reader;
+            SetProfileImage(profileImageHandler(result));
+          };
+        });
+      };
+    
+    
+      const addProfileImage = async ( ) => {
+        
+        let token : any = localStorage.getItem('session');
+        let decoded : any = jwt_decode(token);
+        
+         
+        userService.addProfileImage({ userId : decoded.id , image : profileImageState }).then(( resp : any ) => {
+        try{
+          console.log(resp);
+        }
+        catch(err){
+           console.log(err);
+           
+        }
+      })
+    }
+    
+      useEffect(() => {
+      },[ profileImageState ])
+    
+
+
+
+return (
+<>
+<div className="w-full flex flex-col">
+
+<div className="w-6/12 mx-auto mt-[3rem] flex flex-row justify-between ">
+  <label className=" bg-gradient-to-r from-[#30a362] to-[green]  font-[600] text-[1.5rem] px-5 text-[white] h-[4rem] mt-[3rem] border-none rounded-[10px] boxshadow2 hover:scale-110 duration-200 p-3 transition-all cursor-pointer">
+    <input type="file" className="hidden" onChange={(e) => e.target.files && getBase64(e.target.files[0])}/>
+     Upload +
+  </label>
+
+  <div className="bg-transparent border-[2px] border-solid border-[#353535] h-[10rem] w-[10rem] rounded-[50%] boxshadow3">
+    <img src={profileImageState} className='rounded-[50%]' />
+  </div>
+
+</div>
+
+<button onClick={() => addProfileImage()} className="w-2/12 mx-auto mt-[5rem] bg-gradient-to-r from-[#3073a3] to-[#002980]  font-[600] text-[1.5rem] px-3 text-[white]   border-none rounded-[10px] boxshadow2 hover:scale-105 pb-2 duration-200 p-1 transition-all">Save</button>
+
+     </div> 
+</>
+)
+
+}
+
+
+export default ProfileSettings ; 
--- src/components/Settings/Settings.tsx
+++ src/components/Settings/Settings.tsx
@@ -1,59 +1,13 @@
 import { useEffect, useState } from "react";
 import Header from "../GeneralComponents/Header";
-import { profileImageHandler } from '../../Redux/Reducers/Settings/Profile/ProfileSettings';
-import { useDispatch, useSelector } from "react-redux";
-import userService from "../../api/services/user.service";
-import jwt_decode from "jwt-decode";
+
+import ProfileSettings from "./ProfileSettings/ProfileSettings";
 
 
 const Settings = () => {
    
 const [ showProfileSetting , SetShowProfileSetting ] = useState<boolean>(false);
 
-const selectState :any = useSelector(profileImageHandler);
-const profileImageState = selectState.payload.ProfileSettings.profileImage;
-
-const SetProfileImage = useDispatch();
-
-const getBase64 = ( file : any ) => {
-    return new Promise(resolve => {
-      let baseURL = "";
-      let reader = new FileReader();
-      reader.readAsDataURL(file);
-
-      reader.onload = () => {
-        resolve(baseURL);
-        let {result} : any = reader;
-        SetProfileImage(profileImageHandler(result));
-      };
-    });
-  };
-
-
-  const addProfileImage = async ( ) => {
-    
-    let token : any = localStorage.getItem('session');
-    let decoded : any = jwt_decode(token);
-    
-     
-    userService.addProfileImage({ userId : decoded.id , image : profileImageState }).then(( resp : any ) => {
-    try{
-      console.log(resp);
-    }
-    catch(err){
-       console.log(err);
-       
-    }
-  })
-}
-
-
-
-
-
-
-  useEffect(() => {
-  },[ profileImageState ])
 
 
     return (
@@ -61,30 +15,15 @@ const getBase64 = ( file : any ) => {
 
         <Header/>
       
-     { !showProfileSetting && <div className="w-full flex flex-col justify-around text-[2rem] text-[#8d8d8d] font-[600] tracking-[1px]">
+     { !showProfileSetting &&
+      <div className="w-full flex flex-col justify-around text-[2rem] text-[#8d8d8d] font-[600] tracking-[1px]">
          <div onClick={() => SetShowProfileSetting(true)} className="w-5/12 mx-auto mt-[3rem] bg-[#222225d8] hover:bg-[#1b1b1ed8] transition-all duration-200 cursor-pointer rounded-[10px] pl-6 p-4 boxshadow4">
            Profile
         </div>
-      </div>}
+      </div> }
 
      { showProfileSetting && 
-     <div className="w-full flex flex-col">
-
-<div className="w-6/12 mx-auto mt-[3rem] flex flex-row justify-between ">
-  <label className=" bg-gradient-to-r from-[#30a362] to-[green]  font-[600] text-[1.5rem] px-5 text-[white] h-[4rem] mt-[3rem] border-none rounded-[10px] boxshadow2 hover:scale-110 duration-200 p-3 transition-all cursor-pointer">
-    <input type="file" className="hidden" onChange={(e) => e.target.files && getBase64(e.target.files[0])}/>
-     Upload +
-  </label>
-
-  <div className="bg-transparent border-[2px] border-solid border-[#353535] h-[10rem] w-[10rem] rounded-[50%] boxshadow3">
-    <img src={profileImageState} className='rounded-[50%]' />
-  </div>
-
-</div>
-
-<button onClick={() => addProfileImage()} className="w-2/12 mx-auto mt-[5rem] bg-gradient-to-r from-[#3073a3] to-[#002980]  font-[600] text-[1.5rem] px-3 text-[white]   border-none rounded-[10px] boxshadow2 hover:scale-105 pb-2 duration-200 p-1 transition-all">Save</button>
-
-     </div> }
+     <ProfileSettings/>}
 
         </div>    
     )
