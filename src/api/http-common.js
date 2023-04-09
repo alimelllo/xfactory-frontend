3e601a05a61a42c0a0f08ad51d355ffb5bab353e
Alireza Maleki <mt@gmail.com>
2023-04-09 15:16:23 +0430

    reverting url

--- src/api/http-common.js
+++ src/api/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://xfactory-backend.vercel.app",
+  baseURL: "https://xfactory-backend.vercel.app:8081",
   headers: {
     "Content-type": "application/json",
     "Authorization": "Bearer secret",
