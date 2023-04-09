da9bde9182d121b241f6f043a89829c81e737036
Alireza Maleki <mt@gmail.com>
2023-04-09 15:12:05 +0430

    testing base url config

--- src/api/http-common.js
+++ src/api/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://xfactory-backend.vercel.app:8081",
+  baseURL: "https://xfactory-backend.vercel.app",
   headers: {
     "Content-type": "application/json",
     "Authorization": "Bearer secret",
